9c8a45cbc0684402f8377e302a1e13623002025f
ac2pic <ac2pic@gmail.com>
2019-01-25 20:11:14 -0600

    Fix issue where old title music does not play at the title screen

--- cc-old-title-music/mod.js
+++ cc-old-title-music/mod.js
@@ -1,7 +1,17 @@
 (function() {
 	const onModsLoaded = function() {
-	cc.ig.BGM[entries.titleMusic] = new cc.ig.BGMTrack("title");
-	document.body.removeEventListener("modsLoaded", onModsLoaded);
+		cc.ig.BGM[entries.setTitleMusic] = function() {
+			cc.ig.BGM[entries.titleMusic] = new cc.ig.BGMTrack("menu");
+		};
+		let id = window.setInterval(function() {
+			cc.ig.BGM[entries.setTitleMusic]();
+			if(cc.ig.BGM_TRACK_LIST["menu"]) {
+				clearInterval(id);
+				return;
+			}
+		}, 250);
+		console.debug('Old title music loaded');
+		document.body.removeEventListener("modsLoaded", onModsLoaded);
 	};
 
 	document.body.addEventListener("modsLoaded", onModsLoaded);
