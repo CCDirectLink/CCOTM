707800de61e5bf821adbc2390d35d757ad75e2d7
Sandorik00 <sasha.kobec00@gmail.com>
2020-06-08 14:31:06 +0300

    remove dependency on HCI

--- prestart.js
+++ prestart.js
@@ -1,1 +1,13 @@
-ig.Bgm.preloadStartTrack("title");
+ig.module("impact.feature.bgm.old-title-music")
+  .requires("impact.feature.bgm.bgm")
+  .defines(function () {
+    ig.merge(ig.BGM_TRACK_LIST, {
+      title: {
+        path: "media/bgm/menu.ogg",
+        loopEnd: 36,
+        volume: 0.45,
+      },
+    });
+
+    ig.Bgm.preloadStartTrack("title");
+  });
